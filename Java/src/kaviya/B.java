56a968b0ff1210755eb814fcf6843a0f41dca1e4
Kaviyaulaganathan <kaviyanathan1904@gmail.com>
2026-08-05 21:02:16 -0700

    Initial commit

---
+++ Java/src/kaviya/B.java
@@ -0,0 +1,15 @@
+package kaviya;
+
+public class B {
+		public static void main(String[] args) {
+			int x=2;
+			if(x%2==0) {
+				System.out.println("this is even");	
+		}
+		else {
+			System.out.println("this is odd");
+		}
+
+	}
+	}
+
---
+++ Javaprograms/src/JavaPrograms/EvenNumber.java
@@ -0,0 +1,19 @@
+package JavaPrograms;
+
+public class EvenNumber {
+
+			public static void main(String[] args) {
+				int x=2;
+				if(x%2==0) {
+					System.out.println("this is even");	
+			}
+			else {
+				System.out.println("this is odd");
+			}
+
+		}
+		}
+
+
+
+
