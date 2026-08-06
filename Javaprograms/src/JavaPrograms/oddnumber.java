e8558f4737ac8cda592b9e986c2504f2b26fc585
Kaviyaulaganathan <kaviyanathan1904@gmail.com>
2026-08-06 09:30:41 -0700

    add OddNumber

---
+++ Javaprograms/src/JavaPrograms/oddnumber.java
@@ -0,0 +1,14 @@
+package JavaPrograms;
+
+public class oddnumber {
+	public static void main(String[] args) {
+		int x=7;
+		if(x%2!=0) {
+			System.out.println("this is odd");
+		}
+		else {
+			System.out.println("this is not odd");
+		}
+	}
+
+}
