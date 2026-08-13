6e9ff971b26170ea98fe2f4498463da87e216739
Kaviyaulaganathan <kaviyanathan1904@gmail.com>
2026-08-13 02:42:24 -0700

    Update primenumber.java

--- Javaprograms/src/JavaPrograms/primenumber.java
+++ Javaprograms/src/JavaPrograms/primenumber.java
@@ -1,5 +1,26 @@
 package JavaPrograms;
-
+import java.util.Scanner;
 public class primenumber {
-
-}
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the number");
+		int number=sc.nextInt();
+		boolean res=primenumber(number);
+		if(res) {
+			System.out.println("this is primenumber");
+		}
+		else {
+			System.out.println("this is not primenumber");
+		}
+	}
+		public static boolean primenumber(int number) {
+			int count=0;
+			for(int i=1;i<=number;i++) {
+				if(number%i==0) {
+					count++;
+				}
+			}
+					return count==2;
+		}
+		}
+                                                                                            
